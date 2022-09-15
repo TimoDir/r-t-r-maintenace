4c7327f14fb9a425b9552085ee366349b212833d
TimD <timothee.diroian@gmail.com>
2022-09-15 13:13:35 +0200

    base setup of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import { funnyCat } from './resources/funnycat';
+
+const CatTmblr = ({value, order}) =>{
+  const displayCat = value.map(cat =>{
+    return (
+      <div className="App-img" key={cat.key}>
+        <img src={cat.url} />
+        <h2>{cat.title}</h2>
+        <p>{cat.rate}</p>
+      </div>
+    )
+  });
+  
+  return (
+    <div className='App-Tmblr'>
+      {displayCat}
+    </div>
+  );
+};
 
 function App() {
+  const [catList, setCatList] = React.useState(funnyCat)
+  const [displayParameter, setDisplayParameter] = React.useState('none');
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Header'>
+        <h1>FunnyCat</h1>
+        <p>(^・x・^)</p>
+        <form>
+          <select>
+            <option>none</option>
+            <option>Alphabetic</option>
+            <option>Rating</option>
+          </select>
+        </form>
+      </div>
+      <CatTmblr 
+      value = {catList}
+      order = {displayParameter}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import App from './App';
+import { funnyCat } from './resources/funnycat';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () =>{
+  test('Render a welcoming message', () =>{
+    render(<App />);
+
+  })
+
 });
+
+
+
+describe('funnyCat', () =>{
+  test('Return an array of object', () =>{
+    expect(typeof(funnyCat)).toBe('object');
+    expect(typeof(funnyCat[0])).toBe('object');
+  })
+})
